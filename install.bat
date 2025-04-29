add231655ba03f31c182de54f7c4978095815dc8
praneshnangare <praneshnangare10@gmail.com>
2025-04-30 00:14:00 +0530

    Revert "[MINOR] update functionality updated"
    This reverts commit ee401a010cbc62b41bbda989afcfb4ef7b142b8d.

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-echo Installing dependencies...
-nodejs\node.exe nodejs\node_modules\npm\bin\npm-cli.js install
-pause
--- run-app.bat
+++ run-app.bat
@@ -1,13 +1,26 @@
 @echo off
+echo ================================
+echo   Starting Naukri Application
+echo ================================
+
+REM Move to the folder where the batch file is located
 cd /d %~dp0
 
-REM Check if node_modules folder exists
-if not exist "node_modules" (
-    echo node_modules not found. Running install.bat...
-    call install.bat
+REM Check if node.exe exists
+if not exist node.exe (
+    echo Error: node.exe not found. Please make sure node.exe is in the same folder.
+    pause
+    exit /b
 )
 
-echo Starting the application...
-nodejs\node.exe index.js
+REM Check if node_modules exists, if not install
+if not exist node_modules (
+    echo Installing dependencies...
+    node.exe node_modules/.bin/npm install
+)
+
+REM Run the app
+echo Running the application...
+node.exe index.js
 
 pause
